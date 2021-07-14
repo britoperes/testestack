5593b7829b2ba629dec55b912ba7d1a4d4af6ae5
meli-jobrito <joao.brito@mercadolivre.com>
2021-07-14 16:38:36 -0300

    Criado excecao herdando de Exceptio e RuntimeException (checked e unchecked)

---
+++ src/testeStack/MinhaExcecao.java
@@ -0,0 +1,7 @@
+package testeStack;
+
+public class MinhaExcecao extends Exception {
+	public MinhaExcecao(String s) {
+		super(s);
+	}
+}
--- src/testeStack/TesteStackComTratamento.java
+++ src/testeStack/TesteStackComTratamento.java
@@ -6,23 +6,23 @@ public class TesteStackComTratamento {
 		System.out.println("Inicio da main");
 		try {
 			etapa1();			
-		}catch(ArithmeticException | NullPointerException e) {
+		}catch(ArithmeticException | NullPointerException |MinhaExcecao e) {
 			System.out.println("Exceção - "+e.getMessage());
 		}
 		System.out.println("Termino da main");
 
 	}
 	
-	public static void etapa1() {
+	public static void etapa1() throws MinhaExcecao{
 		System.out.println("Inicio da etapa 1");
 		etapa2();
 		System.out.println("Termino da etapa 1");
 	}
 
-	public static void etapa2() {
+	public static void etapa2() throws MinhaExcecao{
 		System.out.println("Inicio da etapa 2");
 
-		throw new ArithmeticException("Deu problema");
+		throw new MinhaExcecao("Deu problema");
 	}
 
 }
